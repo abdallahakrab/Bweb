e0f98f038c4f4ace9ece43f32ff81aa608b569d0
Abdallah Hossam <abdallahakrab98@gmail.com>
2020-08-27 13:43:51 +0300

    lazyload fix

--- src/app/modules/shared-module/shared-module.module.ts
+++ src/app/modules/shared-module/shared-module.module.ts
@@ -1,12 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Directive,OnInit, EventEmitter, Output, OnDestroy, Input,ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ProfileFormComponent } from '../../components/profile-form/profile-form.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    ProfileFormComponent
+  ],
+  exports:[ProfileFormComponent]
 })
-export class SharedModuleModule { }
+export class SharedModule {}
